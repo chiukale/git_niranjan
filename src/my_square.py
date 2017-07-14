5ffec63539644b1cc5638a0c7f7e494b6f14015c
Niranjan Kale <Niranjan_Kale@rediff.com>
2017-07-15 00:21:06 +0530

    Testing what i have tested

--- src/my_square.py
+++ src/my_square.py
@@ -1,11 +1,7 @@
-def my_square(x):
+def my_square(y):
     """ Removing LCL. Takes a value and returns a square.This is addion to original comments.
-<<<<<<< HEAD
-       Using the ** operator.
-=======
       Getting hands on.
->>>>>>> c37d62150215688f7827f7b5e40f3ede985fab37
     """
-    return(x ** 2)
+    return(y ** 2)
 
-print(my_square(4))
+print(my_square(5))
