b871426f7888a56e9ac12e7614ece07e35e7f17e
Niranjan Kale <Niranjan_Kale@rediff.com>
2017-07-17 15:37:22 +0530

    add and update documentation for squre

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,13 @@
 def my_square(y):
     """ Removing LCL. Takes a value and returns a square.This is addion to original comments.
-      Getting hands on.
+      Getting hands on in ** .
+      pearot
+      using the ** operator
     """
     return(x ** 2)
 print(my_square(4))
 def my_square2(x)
+    """ opertaor to calculate square 2
+    """
 return(x * x)
 print(my_square2(42))
